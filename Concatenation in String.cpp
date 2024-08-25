38c18f6aedb6226dd7d2e1f3b94ae8e72fbf286e
AkshitMathur <mathur.aksh2712@gmail.com>
2024-08-25 12:32:22 +0530

    uploaded concatenation in string

---
+++ Concatenation in String.cpp
@@ -0,0 +1,19 @@
+//23070123014 Akshit Mathur
+#include <iostream>
+using namespace std;
+int main(){
+    string a,b;
+    cout<<"Enter first sring: "<<endl;
+    cin>>a;
+    cout<<"Enter second sring: "<<endl;
+    cin>>b;
+    cout<<"Concatenated String is: "<<a+b<<endl;
+}
+//output
+/*
+Enter first sring: 
+hello
+Enter second sring:
+world
+Concatenated String is: helloworld
+*/
