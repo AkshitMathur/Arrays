e1121f3f62299b8d35c3786cdcd5d56e87f929b8
AkshitMathur <mathur.aksh2712@gmail.com>
2024-08-25 12:09:27 +0530

    Sum and Avg uploaded

---
+++ Sum_and_avg_in array.cpp
@@ -0,0 +1,25 @@
+// 23070123014 Akshit Mathur
+#include <iostream>
+using namespace std;
+int main() {
+    int i,sum=0,avg=0;
+    int x[5];
+    cout<<"enter the elements"<<endl;
+    for (i=0;i<5;i++){
+        cin>>x[i];
+    }
+        for (i=0;i<5;i++){
+            sum=sum+x[i];}
+            cout<<"sum of the elements is:"<<sum<<endl;
+            cout<<"avg of the elements is:"<<sum/5;
+        
+}
+//output
+/*enter the elements
+1
+2
+3
+4
+5
+sum of the elements is:15
+avg of the elements is:3/*
