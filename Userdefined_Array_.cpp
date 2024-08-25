59ab01bd7043422b49c2523e448afbdbfc025886
AkshitMathur <mathur.aksh2712@gmail.com>
2024-08-25 10:31:32 +0530

    uploaded userdefined_array

---
+++ Userdefined_Array_.cpp
@@ -0,0 +1,25 @@
+// 23070123014 Akshit Mathur
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    int x[5];
+    cout<<"enter the elements"<<endl;
+    for (i=0;i<5;i++){
+        cin>>x[i];
+    }
+        for (i=0;i<5;i++){
+            cout<<x[i]<<" ";
+        }
+    
+
+    return 0;
+}
+//output
+/* enter the elements
+1
+2
+3
+4
+5
+1 2 3 4 5 /*
